30a9ae0c29d3afa5209eb2846bfedc8443ff3955
luoxiaokai <xiaokai.luo@baozun.com>
2019-06-12 10:55:29 +0800

    三元数组=0

---
+++ number11.py
@@ -0,0 +1,32 @@
+'''
+给定一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？找出所有满足条件且不重复的三元组。
+
+注意：答案中不可以包含重复的三元组。
+
+例如, 给定数组 nums = [-1, 0, 1, 2, -1, -4]，
+
+满足要求的三元组集合为：
+[
+  [-1, 0, 1],
+  [-1, -1, 2]
+]
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/3sum
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+'''
+
+
+def tripleList(nums):
+    new_list = []
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)):
+            for k in range(j + 1, len(nums)):
+                if nums[i] + nums[j] + nums[k] == 0:
+                    value = nums[i], nums[j], nums[k]
+                    new_list.append(value)
+    return new_list
+
+
+nums = [-1, 0, 1, 2, -1, -4]
+print(tripleList(nums))
