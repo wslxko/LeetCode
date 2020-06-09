0faea417d9aae66300f042876e824cbe2b23c162
luoxiaokai <xiaokai.luo@baozun.com>
2020-06-09 15:04:15 +0800

    add

---
+++ a_daily_topic/多数元素.py
@@ -0,0 +1,33 @@
+'''
+给定一个大小为 n 的数组，找到其中的多数元素。多数元素是指在数组中出现次数大于 ⌊ n/2 ⌋ 的元素。
+
+你可以假设数组是非空的，并且给定的数组总是存在多数元素。
+
+ 
+
+示例 1:
+
+输入: [3,2,3]
+输出: 3
+示例 2:
+
+输入: [2,2,1,1,1,2,2]
+输出: 2
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/majority-element
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+'''
+
+
+class Solution:
+    def majorityElement(self, nums):
+        set_nums = list(set(nums))
+        for num in set_nums:
+            if nums.count(num) > len(nums) / 2:
+                return num
+
+
+a = Solution()
+nums = [2, 2, 1, 1, 1, 2, 2]
+print(a.majorityElement(nums))
